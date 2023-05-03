b1aedc73c8da764083e910929842418f357a3a46
vadymtkach <118903035+vadymtkach@users.noreply.github.com>
2023-05-03 13:24:04 +0200

    chanhe-timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,3 +1,5 @@
+
+
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
@@ -15,9 +17,9 @@ flatpickr(datePicker, {
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  onClose(selectedDates) {
+  onClose: (selectedDates) => {
     const selectedDate = selectedDates[0];
-    const currentDate = new Date();
+    const currentDate = Date.now();
     if (selectedDate <= currentDate) {
       window.alert("Please choose a date in the future");
       startButton.disabled = true;
@@ -28,15 +30,12 @@ flatpickr(datePicker, {
 });
 
 function startTimer() {
-  const selectedDate = new Date(datePicker.value);
-  const currentDate = new Date();
+  const selectedDate = new Date(datePicker.value).getTime();
+  const currentDate = Date.now();
   const timeRemaining = selectedDate - currentDate;
   if (timeRemaining <= 0) {
     clearInterval(intervalId);
-    days.textContent = "00";
-    hours.textContent = "00";
-    minutes.textContent = "00";
-    seconds.textContent = "00";
+    resetTimer();
     return;
   }
   let totalSeconds = Math.floor(timeRemaining / 1000);
@@ -51,40 +50,28 @@ function startTimer() {
     totalSeconds--;
     if (totalSeconds < 0) {
       clearInterval(intervalId);
-      days.textContent = "00";
-      hours.textContent = "00";
-      minutes.textContent = "00";
-      seconds.textContent = "00";
+      resetTimer();
       return;
     }
-    if (totalSeconds % 60 === 59) {
+    if (totalSeconds % 60 === 0) {
       totalMinutes--;
       if (totalMinutes < 0) {
         clearInterval(intervalId);
-        days.textContent = "00";
-        hours.textContent = "00";
-        minutes.textContent = "00";
-        seconds.textContent = "00";
+        resetTimer();
         return;
       }
-      if (totalMinutes % 60 === 59) {
+      if (totalMinutes % 60 === 0) {
         totalHours--;
         if (totalHours < 0) {
           clearInterval(intervalId);
-          days.textContent = "00";
-          hours.textContent = "00";
-          minutes.textContent = "00";
-          seconds.textContent = "00";
+          resetTimer();
           return;
         }
-        if (totalHours % 24 === 23) {
+        if (totalHours % 24 === 0) {
           totalDays--;
           if (totalDays < 0) {
             clearInterval(intervalId);
-            days.textContent = "00";
-            hours.textContent = "00";
-            minutes.textContent = "00";
-            seconds.textContent = "00";
+            resetTimer();
             return;
           }
           days.textContent = pad(totalDays);
@@ -101,4 +88,24 @@ startButton.addEventListener("click", startTimer);
 
 function pad(value) {
   return value.toString().padStart(2, "0");
+}
+
+function resetTimer() {
+  days.textContent = "00";
+  hours.textContent = "00";
+  minutes.textContent = "00";
+  seconds.textContent = "00";
+}
+
+function convertMs(ms) {
+  const totalSeconds = Math.floor(ms / 1000);
+  const totalMinutes = Math.floor(totalSeconds / 60);
+  const totalHours = Math.floor(totalMinutes / 60);
+  const totalDays = Math.floor(totalHours / 24);
+  return {
+    days: totalDays,
+    hours: totalHours % 24,
+    minutes: totalMinutes % 60,
+    seconds: totalSeconds % 60,
+  };
 }
